b0d610da45f8bf183d1be8407f8dcb7c5ead8d19
Boris Serebrov <serebrov@gmail.com>
2016-06-20 10:58:32 +0300

    Resolve conflicts

--- plugins/atom/atom.plugin.zsh
+++ plugins/atom/atom.plugin.zsh
@@ -1,9 +1,23 @@
-# Gets OS Type
-unamestr=$(uname -s)
+if [ Cygwin = "$(uname -o 2>/dev/null)" ]; then
+    local _atom_path > /dev/null 2>&1
 
-# If OSX
-if [[ "$unamestr" == 'Darwin' ]]; then
+    _atom_path=${LOCALAPPDATA}/atom/bin/atom
+
+    if [[ -a $_atom_path ]]; then
+        cyg_open_atom()
+        {
+            if [[ -n $1 ]]; then
+                ${_atom_path} `cygpath -w $1`
+            else
+                ${_atom_path}
+            fi
+        }
+
+        alias at=cyg_open_atom
+    fi
+else
     local _atom_paths > /dev/null 2>&1
+
     _atom_paths=(
         "$HOME/Applications/Atom.app"
         "/Applications/Atom.app"
@@ -15,8 +29,6 @@ if [[ "$unamestr" == 'Darwin' ]]; then
             break
         fi
     done
-# If Linux
-elif [[ "$unamestr" == 'Linux' ]]; then
-    # Alerts the user if 'atom' is not a found command.
-    type atom >/dev/null 2>&1 && alias at="atom" || { echo >&2 "You have enabled the atom oh-my-zsh plugin on Linux, but atom is not a recognized command. Please make sure you have it installed before using this plugin."; }
 fi
+
+alias att='at .'
